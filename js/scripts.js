aeb50c2045d323a67df0a1ddb4a829978daec240
chalmie <student@epicodus.com>
2016-01-15 12:07:04 -0800

    clean up indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,18 @@
-// $(document).ready(function(){
-//   ("").submit(function(event)) {
-//   // When the submit button is clicked,need to
-//   // gather input, convert to numbers,create a score.
-//
-//   alert($('input[name=optradio]').val());
-//   // If points are 5-8, display option1
-//   // Else if points are 9-12,display option2
-//   // Else if points are 13-15,display option3
-//   // Else alert user that there are unanswered questions
-//
-//
-//
-//     event.preventDefault();
-//   });
-// });
-
 
 $(function(){
     $("#submit-button").click(function() {
+      // Assign user input to variables
       var questionOne = parseInt($("input[name=optradio1]:checked").val(),10);
       var questionTwo = parseInt($("input[name=optradio2]:checked").val(),10);
       var questionThree = parseInt($("input[name=optradio3]:checked").val(),10);
       var questionFour = parseInt($("input[name=optradio4]:checked").val(),10);
       var questionFive = parseInt($("input[name=optradio5]:checked").val(),10);
+
+      // Add input variables to create a score
       var score = questionOne + questionTwo + questionThree + questionFour + questionFive;
 
+      // Determine recommendation based on score
+      // or return an error message if questions are left unanswered
       if (score >= 5 && score <= 8) {
         $("#rec-one").siblings().addClass('hidden');
         $("#rec-one").removeClass('hidden');
@@ -37,8 +25,5 @@ $(function(){
       } else {
         alert("Looks like you missed a question!");
       }
-
-
-
     });
 });
